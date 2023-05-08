835b758e51c01c9391f29d803a606b33f28d674c
Leonardo Müller <leowmuller@gmail.com>
2023-05-08 15:18:59 -0300

    exercicio resolvido

--- express-js/exericio/src/index.js
+++ express-js/exericio/src/index.js
@@ -1,19 +1,20 @@
 const express = require(`express`);
-
 const app = express();
 
 const usuarios = [
     { id: 11, nome: 'joao', idade: 23 },
     { id: 2, nome: 'maria', idade: 18 },
     { id: 4, nome: 'ana', idade: 30 },
-    { id: 1, nome: 'rodrigo', idade: 17 }
+    { id: 1, nome: 'rodrigo', idade: 17 },
     { id: 123, nome: 'rodrigo', idade: 17 }
 ];
 
-app.get(`./`, (requisicao, resposta) => {
+app.get(`/`, (requisicao, resposta) => {
     resposta.send(`Rota principal - exercício da aula de express`);
 });
 
-app.get(`./usuarios`, (requisicao, resposta) => {
+app.get(`/usuarios`, (requisicao, resposta) => {
     resposta.send(usuarios);
 });
+
+app.listen(3000);
